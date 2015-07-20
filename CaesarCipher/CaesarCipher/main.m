886333e5a9d40fec59ba0d851f0ce70a0eac7260
Christella <cdolm92@gmail.com>
2015-07-19 23:41:11 -0400

    Homework

--- CaesarCipher/CaesarCipher/main.m
+++ CaesarCipher/CaesarCipher/main.m
@@ -1,60 +1,40 @@
 //
 //  main.m
-//  CaesarCipher
+//  Cypher
 //
-//  Created by Michael Kavouras on 6/21/15.
-//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//  Created by Christella on 7/17/15.
+//  Copyright (c) 2015 Christella Dolmo. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface CaesarCipher : NSObject
-
-- (NSString *)encode:(NSString *)string offset:(int)offset;
-- (NSString *)decode:(NSString *)string offset:(int)offset;
-
-@end
-
-
-@implementation CaesarCipher
-
-- (NSString *)encode:(NSString *)string offset:(int)offset {
-    if (offset > 25) {
-        NSAssert(offset < 26, @"offset is out of range. 1 - 25");
-    }
-    NSString *str = [string lowercaseString];
-    unsigned long count = [string length];
-    unichar result[count];
-    unichar buffer[count];
-    [str getCharacters:buffer range:NSMakeRange(0, count)];
-    
-    char allchars[] = "abcdefghijklmnopqrstuvwxyz";
-
-    for (int i = 0; i < count; i++) {
-        if (buffer[i] == ' ' || ispunct(buffer[i])) {
-            result[i] = buffer[i];
-            continue;
-        }
-        
-        char *e = strchr(allchars, buffer[i]);
-        int idx= (int)(e - allchars);
-        int new_idx = (idx + offset) % strlen(allchars);
-
-        result[i] = allchars[new_idx];
-    }
-
-    return [NSString stringWithCharacters:result length:count];
-}
-
-- (NSString *)decode:(NSString *)string offset:(int)offset {
-    return [self encode:string offset: (26 - offset)];
-}
-
-@end
-
-
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        char str[] = "abcdefghijklmnopqrstuvwxyz";
+        
+        char input;
+        
+        int offset;
+        
+        printf("Enter the offset:");
+        scanf("%d", &offset);
+        
+        
+        printf("Enter a word to offset: ");
+        scanf("%s", &input);
+        
+        
+        
+        
+        
+        for(int i = 0; i < strlen(str); i++)
+        {
+            if(str[i] == input) {
+                
+                printf("%c", str[i + offset]);
+            }
+        }
+        return 0;
     }
 }
